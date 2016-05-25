ff55602bacfe34c9ea9f6de33e1b650656b818de
Sandra <sandra@magdalena>
2016-05-25 15:22:17 +0200

    last updates

--- E_4_mongodb/public/javascript/chat.js
+++ E_4_mongodb/public/javascript/chat.js
@@ -1,27 +1,38 @@
 $(function(){
-
     var socket = io.connect('http://localhost:8080');
 
     socket.on('connect', function(){
         socket.emit('show messages');
     });
-    //emit "send message" event with the input data
+
     $('#submit-button').click(function(){
-        var message = $('#message');
-        socket.emit('send message', message.val());
+        var messageInput = $('#message');
+        socket.emit('send message', messageInput.val());
         $('textarea').val('').empty();
     });
-    //append sent message to chat
-    socket.on('append message', function(msg){
-        var date = new Date();
-        $('#list').append($('<div class= "alert alert-success"' + '<p>'+ date + '</p><p><b>' + msg + '</b></p>' + '</div'));
-    });
-    //show all messages in the database
-    socket.on('chat open', function(msg){
-        $('#list').empty();
-        $.each(msg, function(key, value){
-            $('#list').append($('<div class= "alert alert-success"' + '<p>'+ value.date + '</p><p><b>' + value.message + '</b></p>' + '</div'));
+
+    var renderMessage = function(msg){
+        var messageBody = $('<div>', {
+            class: 'alert alert-success'
         });
+        var publishedDate = $('<p>', {
+            html:msg.date
+        });
+        var publishedMessage = $('<b>', {
+            html: msg.message
+        });
+        messageBody.append(publishedDate, publishedMessage);
+        $('#list').append(messageBody);
+    };
+
+    socket.on('append message', function(msg){
+        renderMessage(msg);
     });
 
+    socket.on('append messages', function(msgs){
+        $('#list').empty();
+        $.each(msgs, function(_, msg){
+            renderMessage(msg);
+        });
+    });
 });
